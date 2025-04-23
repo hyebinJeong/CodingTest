f40edb238babc51873af9808fe9744e87333d2f3
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2025-04-24 01:47:03 +0900

    [level 2] Title: 주식가격, Time: 40.71 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int top = stack.pop();
+                answer[top] = i - top;
+            }
+            stack.push(i);
+        }
+        
+        // 끝까지 가격이 떨어지지 않은 인덱스 처리
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = n - 1 - top;
+        }
+        
+        return answer;
+    }
+}
