3aa5f50c95391e7d33c4e45cfd5010b1028251db
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2025-05-30 00:12:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 1349.63 ms, Memory: 38.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/92334. 신고 결과 받기/README.md
+++ Python3/프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 1355.36 ms
+메모리: 38.4 MB, 시간: 1349.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 21:42:21
+2025년 05월 30일 00:12:52
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ Python3/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,13 +1,12 @@
 def solution(id_list, report, k):
     report_set = set(report)
     answer = [0] * len(id_list)
-    reports = {x : 0 for x in id_list} #신고 받은 횟수 누적 목적
+    reports = {x:0 for x in id_list}
     
     for r in report_set:
-        reports[r.split()[1]] += 1
-    
+        reports[r.split()[1]] += 1 # 신고당한 사람 카운팅 올리기
+        
     for r in report_set:
         if reports[r.split()[1]] >= k:
             answer[id_list.index(r.split()[0])] += 1
-        
     return answer
