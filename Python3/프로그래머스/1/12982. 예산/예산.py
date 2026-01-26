baadc316fe94deaa60b31ce039d7dc5af8de1ec6
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2026-01-26 23:06:07 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/1/12982. 예산/예산.py
+++ Python3/프로그래머스/1/12982. 예산/예산.py
@@ -3,9 +3,9 @@ def solution(d, budget):
     sorted_d = sorted(d)
     sum_d = 0
     
-    for i in sorted_d:
-        sum_d += i
+    for cost in sorted_d:
+        if sum_d + cost > budget:
+            break
+        sum_d += cost
         answer += 1
-        if sum_d > budget:
-            return answer -1
     return answer
