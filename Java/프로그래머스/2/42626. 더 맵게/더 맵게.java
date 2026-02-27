6576756c1a358a5d723e3ea770b1b33c18972ca2
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2026-02-27 16:04:13 +0900

    [level 2] Title: 더 맵게, Time: 1689.00 ms, Memory: 124 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        int answer = 0;
+        
+        while (pq.peek() < K) {
+            if(pq.size() < 2) {
+                return -1;
+            }
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            pq.offer(first + (second * 2));
+            answer += 1;
+        }
+        return answer;
+    }
+}
